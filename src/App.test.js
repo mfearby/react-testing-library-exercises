da7e2ebe5e989307701f9744a3e6c26bce702976
Marc Fearby <marc.fearby@service.nsw.gov.au>
2021-09-04 10:04:05 +1000

    finished first exercise

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders button with a red background and text of "Change to blue"', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByRole('button', { name: 'Change to blue' });
+  expect(button).toHaveStyle('background-color: red');
 });
